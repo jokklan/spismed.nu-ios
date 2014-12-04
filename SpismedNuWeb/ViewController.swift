226e3a1fe9dafafeb1b4ac305a4851f7ca87ac36
Johan Frølich <johanfrolich@gmail.com>
2014-12-04 21:47:43 +0100

    Initial Commit

---
+++ SpismedNuWeb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpismedNuWeb
+//
+//  Created by Johan Frølich on 04/12/14.
+//  Copyright (c) 2014 Pylonweb I/S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SpismedNuWebTests/SpismedNuWebTests.swift
@@ -0,0 +1,36 @@
+//
+//  SpismedNuWebTests.swift
+//  SpismedNuWebTests
+//
+//  Created by Johan Frølich on 04/12/14.
+//  Copyright (c) 2014 Pylonweb I/S. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SpismedNuWebTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
